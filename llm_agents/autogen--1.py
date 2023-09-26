e8f51095e29a341b03275a43077c9139528635c7
kwishna <kwishna@gmail.com>
2023-09-27 03:05:31 +0530

    second commit

--- llm_agents/autogen--1.py
+++ llm_agents/autogen--1.py
@@ -1,5 +1,20 @@
 from autogen import AssistantAgent, UserProxyAgent
-assistant = AssistantAgent("assistant")
-user_proxy = UserProxyAgent("user_proxy")
-user_proxy.initiate_chat(assistant, message="Plot a chart of META and TESLA stock price change YTD.")
-# This initiates an automated chat between the two agents to solve the task
+
+# create an AssistantAgent instance named "assistant"
+assistant = AssistantAgent(name="assistant")
+
+# create a UserProxyAgent instance named "user_proxy"
+user_proxy = UserProxyAgent(name="user_proxy")
+
+# the assistant receives a message from the user, which contains the task description
+user_proxy.initiate_chat(
+    assistant,
+    message="""What date is today? Which big tech stock has the largest year-to-date gain this year? How much is the gain?""",
+)
+
+''''
+    1) The assistant receives a message from the user_proxy, which contains the task description.
+    2) The assistant then tries to write Python code to solve the task and sends the response to the user_proxy.
+    3) Once the user_proxy receives a response from the assistant, it tries to reply by either soliciting human input or preparing an automatically generated reply. If no human input is provided, the user_proxy executes the code and uses the result as the auto-reply.
+    4) The assistant then generates a further response for the user_proxy. The user_proxy can then decide whether to terminate the conversation. If not, steps 3 and 4 are repeated.
+'''
